bbce428b6f24ce20b3e98883bdbd5d846af22ba3
adarsh <adarsh@123>
2022-04-21 13:19:46 +0530

    after ignoring python file

---
+++ sum.py
@@ -0,0 +1,13 @@
+git status
+
+	
+
+
+
+
+
+clera
+clear
+
+s
+[A[A[A[A[C[C[C[B[C[C[C[B[B[[A[A[A[A[A[A[A[A[A[A[A[C[C[C[C[C[C[[A[C[C[C[C[C[C[D [C[C[D> sum.py
